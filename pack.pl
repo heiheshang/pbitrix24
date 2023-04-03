d5938206bd83a984705953fb3a64fc7d43c68f78
heiheshang <strangerfuture2636@gmail.com>
2023-04-03 22:45:50 +0900

    Добавил в pack.pl инфу о репозитории

--- pack.pl
+++ pack.pl
@@ -3,5 +3,4 @@ title('a set of tools for working with Bitrix24 API').
 version('0.1.0').
 author('HeiHeShang', 'strangerfuture@mail.ru').
 
-home('').
-download('').
+home('https://github.com/heiheshang/pbitrix24').
