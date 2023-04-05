f9d532c1d35210478ea44d746d29d2d0a8a36a06
heiheshang <strangerfuture2636@gmail.com>
2023-04-05 11:21:42 +0900

    Версия пакета

--- pack.pl
+++ pack.pl
@@ -1,6 +1,6 @@
 name(bitrix24).
 title('a set of tools for working with Bitrix24 API').
-version('0.1.0').
+version('0.2.0').
 author('HeiHeShang', 'strangerfuture@mail.ru').
 
 home('https://github.com/heiheshang/pbitrix24').
