5432a6f4b086207975b920dfbe55f7488a943c10
HeiHeShang <strangerfuture@mail.ru>
2023-11-28 15:56:16 +0900

    fix cyrilic url

--- pack.pl
+++ pack.pl
@@ -1,6 +1,6 @@
 name(bitrix24).
 title('a set of tools for working with Bitrix24 API').
-version('0.5.0').
+version('0.5.1').
 author('HeiHeShang', 'strangerfuture@mail.ru').
 
 home('https://github.com/heiheshang/pbitrix24').
--- prolog/bitrix24_regapp.pl
+++ prolog/bitrix24_regapp.pl
@@ -5,6 +5,7 @@
 :-use_module(bitrix24_auth).
 :-use_module(bitrix24_request).
 :-use_module(bitrix24_config).
+:-use_module(library(uri)).
 
 install(Data) :-
     bitrix24_auth:assert_keys(Data),
@@ -42,7 +43,7 @@ install(Data) :-
     bitrix24_config:app_info('auth[client_endpoint]', ClientEndpoint),
     forall(config(event, E, H), (
                format(atom(Url), '~w~w~w~w~w~w~w', [ClientEndpoint, 'event.bind.json?auth=', AccessToken, '&event=', E, '&handler=', H]),
-                   get(Url, Reply, [status_code(StatusCode)]),
+               get(Url, Reply, [status_code(StatusCode)]),
                (\+ StatusCode == 200
                 ->
                      debug(log, 'reply ~q : ~q', [Reply, StatusCode])
@@ -64,7 +65,7 @@ install(Data) :-
                memberchk(handler=Handler, Xs),
                format(atom(UrlU), '~wplacement.unbind/?auth=~w&auth_type=0&placement=~w&handler=~w', [ClientEndpoint, AccessToken, Placement, Handler]),
                get(UrlU, ReplyU, []),
-               memberchk(count=1, ReplyU)
+               memberchk(count=_C, ReplyU)
                )
               )
       ;
@@ -75,8 +76,9 @@ install(Data) :-
     bitrix24_config:app_info('auth[access_token]', AccessToken),
     bitrix24_config:app_info('auth[client_endpoint]', ClientEndpoint),
     forall(config(placement, E, [H, T]), (
-               format(atom(Url), '~wplacement.bind/?auth=~w&placement=~w&handler=~w&title=', [ClientEndpoint, AccessToken, E, H, T]),
-                   get(Url, Reply, [status_code(StatusCode)]),
+               format(atom(Url), '~wplacement.bind/?auth=~w&placement=~w&handler=~w&title=~w', [ClientEndpoint, AccessToken, E, H, T]),
+               uri:urinormalized(Url, UrlNormalized),
+               get(UrlNormalized, Reply, [status_code(StatusCode)]),
                (\+ StatusCode == 200
                 ->
                      debug(log, 'reply ~q : ~q', [Reply, StatusCode])
